65dde6b2d8a712e0d5d2bb2109be14c731cd2e08
Brandon Bertelsen <brandon@bertelsen.ca>
2026-06-16 15:50:31 -0400

    workflows: public surface

--- packages/workflows/src/index.ts
+++ packages/workflows/src/index.ts
@@ -1,1 +1,28 @@
-export {};
+export type {
+  StageRole,
+  Gate,
+  Action,
+  Harness,
+  SessionMode,
+  AgentSettings,
+  CompletionOutput,
+  DirtyTreePolicy,
+  Stage,
+  WorkflowSpec,
+} from "./types";
+export {
+  STAGE_ROLES,
+  GATES,
+  ACTIONS,
+  IRREVERSIBLE_ACTIONS,
+  isStageRole,
+  isGate,
+  isAction,
+} from "./catalog";
+export { DEFAULT_WORKFLOW } from "./default";
+export { parseWorkflowMarkdown } from "./parse";
+export type { ParsedStage, ParsedWorkflow } from "./parse";
+export { validateWorkflow } from "./validate";
+export type { ValidationResult } from "./validate";
+export { resolveSpec } from "./resolve";
+export type { StageOverride, WorkflowOverride } from "./resolve";
